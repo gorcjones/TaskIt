c5ba1ab5d284b83502d3f0d54c57cf6876dd0288
rcjones <rcjones.iphone@gmail.com>
2015-05-28 11:24:56 -0500

    Built taskArray to store data and present that data in the tableviewcell from the array.

--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -12,10 +12,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var tableView: UITableView!
     
+    var taskArray:[Dictionary<String,String>] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let task1:Dictionary<String, String> = ["task": "Study French", "subtask": "Verbs", "date": "01/14/2014"]
+        let task2:Dictionary<String, String> = ["task": "Eat Dinner", "subtask": "Burgers", "date": "01/15/2014"]
+        let task3:Dictionary<String, String> = ["task": "Gym", "subtask": "Leg day", "date": "01/16/2014"]
+        println(task1["task"])
+        println(task1["date"])
+        taskArray = [task1, task2, task3]
+        
+        self.tableView.reloadData()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,15 +35,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     //UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return self.taskArray.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         println(indexPath.row)
+        
+        let taskDict:Dictionary = taskArray[indexPath.row]
+        
         var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell") as! TaskCell
-        cell.taskLabel.text = "Study French"
-        cell.descriptionLabel.text = "Verbs in past and present"
-        cell.dateLabel.text = "01/10/2014"
+        cell.taskLabel.text = taskDict["task"]
+        cell.descriptionLabel.text = taskDict["subtask"]
+        cell.dateLabel.text = taskDict["date"]
         
         return cell
         
