5072226524266fd0a93a161ff0a5c2a10a7ec511
rcjones <rcjones.iphone@gmail.com>
2015-05-30 13:13:47 -0500

    Setup new TaskModel in CoreData

--- TaskIt/TaskModel.swift
+++ TaskIt/TaskModel.swift
@@ -2,15 +2,19 @@
 //  TaskModel.swift
 //  TaskIt
 //
-//  Created by Ryan Jones on 5/28/15.
+//  Created by Ryan Jones on 5/30/15.
 //  Copyright (c) 2015 Ryan Jones. All rights reserved.
 //
 
 import Foundation
+import CoreData
 
-struct TaskModel {
-    var task:String
-    var subTask:String
-    var date:NSDate
-    var completed:Bool
+@objc(TaskModel)
+class TaskModel: NSManagedObject {
+
+    @NSManaged var completed: NSNumber
+    @NSManaged var date: NSDate
+    @NSManaged var subtask: String
+    @NSManaged var task: String
+
 }
